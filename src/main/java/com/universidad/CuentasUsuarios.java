3832c899401a12036371e9d97dfa068ec09b0013
aguustin <94425896+aguustin@users.noreply.github.com>
2022-07-17 22:49:36 -0300

    Create CuentasUsuarios.java

---
+++ src/main/java/com/universidad/CuentasUsuarios.java
@@ -0,0 +1,12 @@
+ @Getter @Setter
+    private String username;
+
+    @Getter @Setter
+    private String contrasena;
+
+    public CuentasUsuarios(String username, String contrasena) {
+        this.username = username;
+        this.contrasena = contrasena;
+    }
+
+}
